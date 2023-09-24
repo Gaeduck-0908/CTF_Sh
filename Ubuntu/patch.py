785628567bbf7407249c6d88eec0e05aeae1ea86
KimTaeHan <82009667+Gaeduck-0908@users.noreply.github.com>
2023-09-24 20:05:25 +0900

    Delete Ubuntu directory

--- Ubuntu/patch.py
+++
@@ -1,7 +0,0 @@
-print("patch file offset num")
-from sys import argv
-
-for i in range(int(argv[3])):
-    with open(argv[1], "r+b") as f:
-        f.seek(int(argv[2], 16) + i)
-        f.write(b"\x90")
